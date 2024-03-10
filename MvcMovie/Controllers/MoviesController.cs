c703e6e7378ae50a13ed13c5e7647bf6a50a9a8b
Darien Weller <darien.r.weller@gmail.com>
2024-03-09 19:40:26 -0700

    Quick Change

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -121,7 +121,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating,ImageName")] Movie movie)
         {
             if (id != movie.Id)
             {
